8a13862b4fd4e5ce5d3879b1ffed3db8e74e0989
Yujie_Liu <136684976+RCrobotcat@users.noreply.github.com>
2026-06-07 23:22:51 +0800

    added main menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button startButton;
+    public Button websiteButton;
+    public string websiteURL = "https://sovereigncore-web.rcrobotcat.workers.dev/";
+    public Button exitButton;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(OnStartClicked);
+        websiteButton.onClick.AddListener(OnWebsiteClicked);
+        exitButton.onClick.AddListener(OnExitClicked);
+    }
+
+    private void OnStartClicked()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    private void OnWebsiteClicked()
+    {
+        Application.OpenURL(websiteURL);
+    }
+
+    private void OnExitClicked()
+    {
+        Application.Quit();
+    }
+}
